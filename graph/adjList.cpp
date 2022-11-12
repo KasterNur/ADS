54e9b3d084aaf11957c6cff3f8d8e843452b53cd
Kaster <h_hans51@hotmail.com>
2022-11-12 14:13:05 +0600

    ADS examples

---
+++ graph/adjList.cpp
@@ -0,0 +1,32 @@
+#include <bits\stdc++.h>
+using namespace std;
+
+void addEdge(vector<int> adj[],int s, int d){
+    adj[s].push_back(d);
+     adj[d].push_back(s);
+}
+
+void printGraph(vector<int> adj[], int V)
+{
+    for (int  i = 0; i < V; i++)
+    {
+        cout<<"\n Vertix"<<i<<" :";
+     
+    for (int j=0;j<adj[i].size(); j++)
+      cout << "-> " << adj[i][j];
+    printf("\n");
+    }
+    
+}
+
+int main(){
+
+   int V=5;
+   vector<int> adj[V];
+  addEdge(adj, 0, 1);
+  addEdge(adj, 0, 2);
+  addEdge(adj, 0, 3);
+  addEdge(adj, 1, 2);
+    printGraph(adj,V);
+    return 0;
+}
