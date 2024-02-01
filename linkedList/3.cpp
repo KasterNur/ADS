d4ddc22593e38991da36efe88a13bcdf62ff0189
KasterNur <h_hans51@hotmail.com>
2024-02-01 16:11:39 +0600

    Update 3.cpp

--- linkedList/3.cpp
+++ linkedList/3.cpp
@@ -1,134 +1,98 @@
 #include <iostream>
-
 using namespace std;
 
-
-struct node{
-    int data;
-    struct node* next;
-
-    node(){
-        this->data = 0;
-        this->next = NULL;
-    }
-    node(int data){
-        this->data = data;
-        this->next = NULL;
-    }
-    node(int data, node* next){
-        this->data = data;
-        this->next = next;
-    }
+class Node{
+    public:
+        int data;
+        Node *next;
+        Node *prov;
 };
-struct Linked_list{
-    node* head;
 
+void insertBfore(Node *thePointer, int value)
+{
+    Node* newNode = new Node();
+    newNode->data = value;
+    Node * tem = new Node();
+    tem = thePointer;
+    thePointer->next = newNode;
+    newNode->next=tem;
+     
     
-    Linked_list()
+}
+
+void insertAfter(Node *thePointer, int value)
+{
+    if(thePointer==NULL)
     {
-        head = new node();
+        cout<<"the pointer can't be NULL"<<endl;
+        return;
     }
-    int front(){
-        return head->data;
+    Node *newNode = new Node();
+    newNode->data = value;
+    newNode->next = thePointer->next;
+    thePointer->next = newNode;
+}
+
+void push(Node **theP, int value)
+{
+    Node *newNode = new Node();
+    newNode->data = value;
+    newNode->next = *theP;
+    newNode->prov = newNode;
+    *(theP) = newNode;
+}
+
+void printAll(Node *theP)
+{
+        if(theP==NULL)
+    {
+        cout<<" IT is NULL";
+    }
+    while(theP!=NULL)
+    {
+        cout<<theP->data<<"--->";
+        theP = theP->next;
     }
     
-    void push_front(int x){
-        if (head == NULL){
-            head = new node(x);
-            return;
-        }
-        node* v = new node(x,head);
-        head =v;
-    }
-    void pop_front(){
-        if(head ==NULL){
-            cerr<<"Error: List is empty\n";
-            exit(0);
-        }
-        node* newHead = head->next;
-        delete head; // avoid memory leak
-        head = newHead;
-    }
-    node* get_tail(){
-        node* tail = head;
-        while(tail!=NULL){
-            tail = tail->next;
-        }
-        return tail;
-    }
-    int back(){
-        return get_tail()->data;
-    }
-    void push_back(int x){
-        if (head == NULL){
-            head = new node(x);
-            return;
-        }
-        node* tail = get_tail();
-        node* v = new node(x);
-        tail->next = v;
-    }
-    void pop_back(){
-        if(head == NULL){
-            cerr<<"Error: List is empty\n";
-            exit(0);
-        }
-        if(head->next ==NULL){
-            //forget about head
-            delete head;
-            head = NULL;
-            return;
-        }
-        // pick the element that preceeds tail
-        node* pre_tail = head;
-        while(pre_tail->next->next!=NULL){
-            pre_tail=pre_tail->next;
-        }
-        node* tail = pre_tail->next;
-        pre_tail->next = NULL;
-        delete tail;
-    }
-    void print(){
-        node* v =head;
-        cout<<"[";
-        while(v!=NULL){
-            cout<<v->data;
-            if(v->next != NULL){
-                cout<<" ";
-            }
-            else cout<<"]\n";
-            v = v->next;
-        }
-        
-    }
-};
-int main(){
-    int tests; 
-    cin>>tests;
-    Linked_list L=  Linked_list();
-    for (int i = 0; i<tests; i++) {
-        string s;
-        cin>>s;
-        int x;
-        if(s == "push_back"){
-            cin>>x;
-            L.push_back(x);
-            L.print();
-            cout << "OK\n";
-        }
-        if(s == "push_front"){
-            cin>>x;
-            L.push_front(x);
-            cout << "OK\n";
-        }
-        if(s == "pop_front"){
-            L.pop_front();
-            cout<<"OK\n";
-        }
-        if(s == "pop_back"){
-            L.pop_back();
-            cout<<"OK\n";
-        }
-        L.print();
-    }
+    cout<<endl;
+  
+}
+
+void del(Node *theP)
+{
+    Node * tem = new Node();
+    Node * next = new Node();
+   tem = theP;
+   next = theP->next;
+   cout<<"current data="<<theP->data<<" next data="<<next->data<<endl;
+    tem->next = theP->next;
+    delete(tem);
+    tem->next = next->next;
+    
+}
+int main() {
+
+
+    Node *head =NULL;
+      
+    push(&head, 1);
+    push(&head, 2);
+    push(&head, 3);
+    push(&head, 4);
+    Node *midle4 = new Node();
+    midle4 = head;
+    push(&head, 5);
+    push(&head, 6);
+    push(&head, 7);
+    push(&head, 8);
+    
+    printAll(head);
+    
+   insertAfter(midle4,0);
+    printAll(head);
+   // insertBfore(midle4,99);
+    //printAll(head);
+    del(midle4);
+    printAll(head);
+    return 0;
 }
